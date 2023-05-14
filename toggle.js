94d522c152ef8943a659b9c7b6ca8f62ea1a4eb9
milan <1234mrego@gmail.com>
2023-05-14 19:53:32 +0530

    initial commit

---
+++ toggle.js
@@ -0,0 +1,12 @@
+let myToggler = document.querySelector(".nav__toggler");
+let myNav = document.querySelector(".nav__menu");
+
+myToggler.addEventListener("click", function(){
+    if(myNav.classList.contains("nav__menu")){
+        myNav.classList.replace("nav__menu", "nav__collapsible");
+    }
+    else
+    {
+        myNav.classList.replace("nav__collapsible", "nav__menu");
+    } 
+});
